370ff5947e3846de73585ddae92fd4ad1c223dbd
ofgulban <farukgulban@gmail.com>
2020-01-22 11:18:48 +0100

    initial push

---
+++ slowest_particle_simulator_on_earth/utils.py
@@ -0,0 +1,21 @@
+"""Utility functions and procedures."""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from os.path import join
+
+
+def save_img(img, out_dir, suffix="", invert=False):
+    """Save a grid scalar field as png file."""
+    img_out = np.copy(img)
+    img_out[img < 0] = 0
+    img_out[img > 1] = 1
+    img_out = np.repeat(img_out[..., None], 3, axis=2)  # For greyscale RGB
+
+    if invert:
+        img_out *= -1
+        img_out += 1
+
+    out_name = "test_{}.png".format(suffix)
+    out_path = join(out_dir, out_name)
+    plt.imsave(out_path, img_out, origin="upper")
