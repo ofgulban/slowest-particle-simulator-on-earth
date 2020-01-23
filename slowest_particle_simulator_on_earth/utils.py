e2f004994e0a37c6a29f01a9d472b7b13a10f550
ofgulban <farukgulban@gmail.com>
2020-01-23 11:52:40 +0100

    add new utility function

--- slowest_particle_simulator_on_earth/utils.py
+++ slowest_particle_simulator_on_earth/utils.py
@@ -33,3 +33,25 @@ def create_export_folder(nii_filename):
 
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
     return output_dir
+
+
+def embed_data_into_square_lattice(data):
+    """Insert MR image into square 2D array."""
+    dims = np.array(data.shape)
+
+    offset_x = int((dims.max() - dims[0]) / 2.)
+    offset_y = int((dims.max() - dims[1]) / 2.)
+
+    temp = np.zeros((dims.max(), dims.max()))
+    temp[offset_x:offset_x+dims[0], offset_y:offset_y+dims[1]] = data
+    return temp
+
+
+def normalize_data_range(data, thr_min=0, thr_max=500):
+    """Negative numbers become 0 and normalize non-zero data into 0-1 range."""
+    data -= thr_min
+    data[data < 0] = 0
+    data[data > (thr_max - thr_min)] = thr_max - thr_min
+    data = data / (thr_max - thr_min)
+    data *= 0.5
+    return data
