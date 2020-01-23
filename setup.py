fb81374a30c04a6f68b016f33fea3afd9ba4c418
ofgulban <farukgulban@gmail.com>
2020-01-23 15:05:02 +0100

    pypi readme is not working for some reason, reverted

--- setup.py
+++ setup.py
@@ -13,29 +13,21 @@ Notes for PyPI:
 4. ```python setup.py sdist upload -r pypi```
 """
 
-import os
 from setuptools import setup
 
-# read the contents of README.md
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    LONG_DESCRIPTION = f.read()
-
-setup(name='slowest_particle_simulator_on_earth',
-      version='0.0.2',
-      description=('A very slow particle simulator for exploding nifti files.'),
-      long_description=LONG_DESCRIPTION,
-      long_description_content_type='text/markdown',
-      url='https://github.com/ofgulban/slowest-particle-simulator-on-earth',
-      author='Omer Faruk Gulban',
-      author_email="farukgulban@gmail.com",
-      license='BSD-3-clause',
-      packages=['slowest_particle_simulator_on_earth'],
-      install_requires=['numpy>=1.17', 'matplotlib>=3.1', 'nibabel>=2.2'],
-      keywords=['mri', 'nifti', 'particle', 'voxel', 'simulation', 'explosion'],
-      zip_safe=True,
-      entry_points={
-        'console_scripts': [
-            'slowest_particle_simulator_on_earth = slowest_particle_simulator_on_earth.__main__:main',
-            ]},
-      )
+setup(
+    name="slowest_particle_simulator_on_earth",
+    description=("A very slow particle simulator for exploding nifti files."),
+    license="BSD-3-clause",
+    version="0.0.6",
+    url="https://github.com/ofgulban/slowest-particle-simulator-on-earth",
+    author="Omer Faruk Gulban",
+    packages=["slowest_particle_simulator_on_earth"],
+    install_requires=["numpy>=1.17", "matplotlib>=3.1", "nibabel>=2.2"],
+    keywords=["mri", "nifti", "particle", "voxel", "simulation", "explosion"],
+    zip_safe=True,
+    entry_points={
+    "console_scripts": [
+        "slowest_particle_simulator_on_earth = slowest_particle_simulator_on_earth.__main__:main",
+        ]},
+    )
