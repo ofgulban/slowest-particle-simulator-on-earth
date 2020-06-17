860d0d2ecc71e22551cdc301569c41c67beb8a57
ofgulban <farukgulban@gmail.com>
2020-06-17 15:38:51 +0200

    Update

--- script_examples/06_OHBM2020.py
+++ script_examples/06_OHBM2020.py
@@ -1,6 +1,5 @@
 """Script example 5: read png."""
 
-import nibabel as nb
 import numpy as np
 import cv2
 from scipy.ndimage import zoom
@@ -14,31 +13,36 @@ from slowest_particle_simulator_on_earth.utils import (
 
 # =============================================================================
 # Parameters
-PNG_FILE1 = "/home/faruk/gdrive/test_brainsplode2/ohbm/step_01.png"
-PNG_FILE2 = "/home/faruk/gdrive/test_brainsplode2/ohbm/step_02.png"
+PNG_FILE1 = "/home/faruk/gdrive/test_brainsplode2/ohbm/OHBM2020_1.png"
+PNG_FILE2 = "/home/faruk/gdrive/test_brainsplode2/ohbm/g982.png"
 OUT_DIR = create_export_folder(PNG_FILE1)
 
-NR_ITER = 400
+NR_ITER = 200
 
 DT = 1  # Time step (smaller = more accurate simulation)
 GRAVITY = 0.2
 
 # =============================================================================
-# Load png
+# Load png (BGR)
 data_in_1 = cv2.imread(PNG_FILE1)
 data_in_1 = np.asarray(data_in_1, dtype=float)
 data_in_2 = cv2.imread(PNG_FILE2)
 data_in_2 = np.asarray(data_in_2, dtype=float)
 
-# Select a channel and downsample
-data_1 = zoom(data_in_1[:, :, 2], 1/8, mode="nearest", prefilter=False)
-data_2 = zoom(data_in_2[:, :, 2], 1/8, mode="nearest", prefilter=False)
+# Select a channel
+data_1_moving = data_in_1[:, :, 2]
+data_1_static = data_in_1[:, :, 1]
+data_1_bounce = data_in_1[:, :, 0]
+
+# Downsample
+data_1_moving = zoom(data_1_moving, 1/2, mode="nearest", prefilter=False)
+data_1_static = zoom(data_1_static, 1/2, mode="nearest", prefilter=False)
+data_1_bounce = zoom(data_1_bounce, 1/2, mode="nearest", prefilter=False)
 
 # Normalize to 0-0.5 range
-data_1 = normalize_data_range(data_1, thr_min=0, thr_max=255)*2
-data_2 = normalize_data_range(data_2, thr_min=0, thr_max=255)*2
+data_1_moving = normalize_data_range(data_1_moving, thr_min=0, thr_max=255)*2
 
-static = np.where(data_2)
+static = np.where(data_1_static)
 
 # Binarize
 # data = ~(data > 0)
@@ -48,15 +52,18 @@ static = np.where(data_2)
 
 # =============================================================================
 # Initialize particles
-x, y = np.where(data_1)
+x, y = np.where(data_1_moving)
 p_pos = np.stack((x, y), axis=1)
 p_pos = p_pos.astype(float)
 
 NR_PART = p_pos.shape[0]
 
 # Record voxel values into particles
-p_vals = data_1[x, y]
+p_vals = data_1_moving[x, y]
 x, y = None, None
+
+# Bounce voxels
+idx_mask_x, idx_mask_y = np.where(data_1_bounce != 0)
 
 # Move particles to the center of cells
 p_pos[:, 0] += 0.5
@@ -71,7 +78,7 @@ p_mass = np.ones(NR_PART)
 p_C = np.zeros((NR_PART, 2, 2))
 
 # Initialize cells
-cells = np.zeros(data_1.shape)
+cells = np.zeros(data_1_moving.shape)
 
 # Some informative prints
 log_welcome()
@@ -96,9 +103,12 @@ for t in range(NR_ITER):
     c_velo = grid_velocity_update(
         c_velo, c_mass, dt=DT, gravity=GRAVITY)
 
+    # Manipulate grid velocities
+    c_velo[idx_mask_x, idx_mask_y, :] *= -0.75
+
     p_pos, p_velo = grid_to_particle_velocity(
         p_pos, p_velo, p_weights, c_velo, dt=DT,
-        rule="slip", bounce_factor=-1)
+        rule="bounce", bounce_factor=-1.5)
 
     # Adjust brightness w.r.t. mass
     c_values[c_mass > 2] /= c_mass[c_mass > 2]
